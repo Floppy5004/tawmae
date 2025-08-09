60ffe40ae088ac3fe1f9aa100fe7dc4fbf966ac4
tawmae <168794128+tawmae@users.noreply.github.com>
2025-08-09 02:59:10 +0200

    Update extension-script.js

--- assets/js/extension-script.js
+++ assets/js/extension-script.js
@@ -86,11 +86,7 @@ document.addEventListener("DOMContentLoaded", function () {
         loadImportString("/action-imports/rotator.txt");
     } else if (currentPage.includes("stream-receipt")) {
         loadImportString("/action-imports/stream-receipt.txt");
-    }else if (currentPage.includes("spotify-and-sb-legacy")) {
-        loadImportString("/action-imports/spotify-and-sb-legacy.txt");
-    }
-
- else if (currentPage.includes("spotify-and-sb")) {
+    }else if (currentPage.includes("spotify-and-sb")) {
         loadImportString("/action-imports/spotify-and-sb.txt");
     } else if (currentPage.includes("bluesky-and-sb")) {
         loadImportString("/action-imports/bluesky-and-sb.txt");
